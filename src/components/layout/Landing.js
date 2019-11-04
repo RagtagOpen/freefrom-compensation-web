c00e98a809da6546ac2d728c0f681bad779175c3
Grant Stromgren <grant@grantstromgren.com>
2019-11-03 18:51:59 -0600

    Updates from comments

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -8,14 +8,14 @@ import { setAgreement, setCookies } from "../../actions/quizActions"
 
 // Material UI
 import Button from "@material-ui/core/Button"
-import MaterialLink from "@material-ui/core/Link"
 import Typography from "@material-ui/core/Typography"
 import Container from "@material-ui/core/Container"
 import FormGroup from "@material-ui/core/FormGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Checkbox from "@material-ui/core/Checkbox"
-import Drawer from "@material-ui/core/Drawer"
-import Grid from "@material-ui/core/Grid"
+
+// Components
+import CookiesConsent from "./CookiesConsent"
 
 const Landing = ({ isAuthenticated, quiz, setAgreement, setCookies }) => {
   if (isAuthenticated) {
@@ -67,54 +67,7 @@ const Landing = ({ isAuthenticated, quiz, setAgreement, setCookies }) => {
       >
         Start
       </Button>
-      {quiz.cookies === null && (
-        <Drawer anchor="bottom" open={quiz.cookies === null}>
-          <Container maxWidth="md">
-            <Grid
-              container
-              spacing={3}
-              direction="row"
-              alignItems="center"
-              justify="space-between"
-            >
-              <Grid item xs={12}>
-                <Typography color="secondary" align="center">
-                  This website uses cookies to ensure you get the best
-                  experience on our website.{" "}
-                  <MaterialLink
-                    color="secondary"
-                    href="https://www.cookiesandyou.com/"
-                  >
-                    Learn more
-                  </MaterialLink>
-                </Typography>
-              </Grid>
-              <Grid container item xs={6} justify="flex-end">
-                <Button
-                  color="secondary"
-                  variant="outlined"
-                  onClick={() => {
-                    setCookies(false)
-                  }}
-                >
-                  Decline
-                </Button>
-              </Grid>
-              <Grid container item xs={6} justify="flex-start">
-                <Button
-                  color="secondary"
-                  variant="contained"
-                  onClick={() => {
-                    setCookies(true)
-                  }}
-                >
-                  Accept
-                </Button>
-              </Grid>
-            </Grid>
-          </Container>
-        </Drawer>
-      )}
+      {quiz.cookies === null && <CookiesConsent />}
     </Container>
   )
 }
