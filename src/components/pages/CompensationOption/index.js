84b1ae0f633beca034c4ad32db5db0466d6225e2
Emily Giurleo <e.m.giurleo@gmail.com>
2019-11-14 23:58:04 -0500

    navigate between sections

--- src/components/pages/CompensationOption/index.js
+++ src/components/pages/CompensationOption/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { connect } from "react-redux"
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"
 
 // Material UI
 import {
@@ -9,15 +9,15 @@ import {
   Container,
   Typography,
   CardMedia,
-  Grid,
+  Link,
   makeStyles,
 } from "@material-ui/core"
 
 // Helpers
-import { CODES_TO_STATES } from 'utils/helpers'
+import { CODES_TO_STATES } from "utils/helpers"
 
 // Components
-import TheDetails from './TheDetails'
+import TheDetails from "./TheDetails"
 import { Title } from "components/layout"
 import Image from "images/resources/victims-of-crime-act.png"
 
@@ -28,12 +28,20 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const CompensationOption = ({}) => {
-  const { section, state, slug } = useParams();
-  debugger;
-  const classes = useStyles();
+  const { section: currentSection, state, slug } = useParams()
+  const classes = useStyles()
+
+  const sections = [
+    "The Details",
+    "The Challenges",
+    "How to Apply",
+    "What to Expect After You Apply",
+    "What if I don't Agree with the Judge's Decision?",
+    "Resources",
+  ]
 
   const renderContent = () => {
-    switch (section) {
+    switch (currentSection) {
       case "1":
         return <TheDetails />
       case "2":
@@ -42,27 +50,52 @@ const CompensationOption = ({}) => {
     }
   }
 
-  return(
+  return (
     <Container maxWidth="md">
       <Title />
-      <Typography variant={"h2"}>For {CODES_TO_STATES[state]} Residents</Typography>
+      <Typography variant={"h2"}>
+        For {CODES_TO_STATES[state]} Residents
+      </Typography>
 
       <Box display="flex" flexDirection="column" alignItems="center">
-        <CardMedia
-          component="img"
-          src={Image}
-          className={classes.image}
-        />
+        <CardMedia component="img" src={Image} className={classes.image} />
         <Typography variant={"h2"}>VICTIMS OF CRIME ACT</Typography>
       </Box>
 
       {renderContent()}
+
+      <Box display="flex" justifyContent="center">
+        <Button disabled={currentSection == "7"}>Next Section</Button>
+      </Box>
+
+      <Typography variant={"h3"}>JUMP TO SECTION</Typography>
+      {sections.map((section, idx) => {
+        return idx + 1 == currentSection ? (
+          <Typography variant={"paragraph"}>
+            <>
+              {idx + 1}. {section}
+              <br />
+            </>
+          </Typography>
+        ) : (
+          <>
+            <Link href={`/compensations/${state}/${slug}/sections/${idx + 1}`}>
+              {idx + 1}. {section}
+            </Link>
+            <br />
+          </>
+        )
+      })}
+
+      {/* TODO: this button should probably be conditionally displayed?? */}
+      <Box display="flex" justifyContent="center">
+        <Button>Back To Results</Button>
+      </Box>
     </Container>
   )
 }
 
-const mapStateToProps = () => ({
-})
+const mapStateToProps = () => ({})
 
 export default connect(
   mapStateToProps,
