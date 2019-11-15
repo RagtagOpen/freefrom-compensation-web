beef27c8562eaad9ed6731675fb9450c4333da78
Emily Giurleo <e.m.giurleo@gmail.com>
2019-11-14 22:22:52 -0500

    add subtitle

--- src/components/pages/CompensationOption.js
+++ src/components/pages/CompensationOption.js
@@ -1,21 +1,29 @@
 import React from "react"
 import { connect } from "react-redux"
+import { useParams } from "react-router-dom";
 
 // Material UI
 import {
   Box,
   Button,
   Container,
+  Typography,
   Grid,
 } from "@material-ui/core"
+
+// Helpers
+import { CODES_TO_STATES } from 'utils/helpers'
 
 // Components
 import { Title } from "components/layout"
 
 const CompensationOption = ({}) => {
+  const { state, slug } = useParams();
+
   return(
     <Container maxWidth="md">
       <Title />
+      <Typography variant={"h2"}>For {CODES_TO_STATES[state]} Residents</Typography>
     </Container>
   )
 }
